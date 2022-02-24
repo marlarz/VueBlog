b6a0f2a56e2d4c50ca22068f28302508d63bf16f
Marcin Pacholarz <40630360+marlarz@users.noreply.github.com>
2022-02-24 16:28:35 +0100

    Update index.js

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -5,7 +5,7 @@ export class Http {
     this.axios = axios.create({
       headers: {
         'Content-type': 'application/json;charset=UTF-8',
-        Authorization: 'Bearer kl_GyXSju2GsgzDtOL-LC3f46KczzfWHHTZ4e-jY66o',
+        Authorization: 'Bearer ',
       },
     });
     // this.axios.interceptors.response.use((response) => {
